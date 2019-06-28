eb53019edaaf3c2b0f1fb67e3604275cbcf72063
Toshihiro Kobayashi <koba@sigfoss.com>
2019-06-29 02:22:12 +0900

    Disable unsupported input size (512)

--- ssd.py
+++ ssd.py
@@ -183,15 +183,15 @@ def multibox(vgg, extra_layers, cfg, num_classes):
 base = {
     '300': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'C', 512, 512, 512, 'M',
             512, 512, 512],
-    '512': [],
+#    '512': [],
 }
 extras = {
     '300': [256, 'S', 512, 128, 'S', 256, 128, 256, 128, 256],
-    '512': [],
+#    '512': [],
 }
 mbox = {
     '300': [4, 6, 6, 6, 4, 4],  # number of boxes per feature map location
-    '512': [],
+#    '512': [],
 }
 
 
@@ -199,7 +199,7 @@ def build_ssd(phase, size=300, num_classes=21):
     if phase != "test" and phase != "train":
         print("ERROR: Phase: " + phase + " not recognized")
         return
-    if size != 300:
+    if not str(size) in base:
         print("ERROR: You specified size " + repr(size) + ". However, " +
               "currently only SSD300 (size=300) is supported!")
         return
